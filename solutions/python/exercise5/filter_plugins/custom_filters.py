6dcf7f13d5b67a6f1e6b7aea81b731adb37c8dc7
Gerasimos Tzakis <gerasimos.tzakis@networktocode.com>
2022-08-19 12:33:44 +0000

    python exercise 5

---
+++ solutions/python/exercise5/filter_plugins/custom_filters.py
@@ -0,0 +1,21 @@
+"""Ansible custom jinja2 filters"""
+#!/usr/bin/python
+class FilterModule():
+    def filters(self):
+        """Ansible filters """
+        return {"expand_interfaces": self.expand_interfaces}
+
+    def expand_interfaces(self, interfaces, profiles):
+        """Exapnd interfaces info from profiles
+
+        Args:
+            interfaces (list): list of dicts with interface info
+            profiles (dict): dict of dicts for every profile
+
+        Returns:
+            list: list of dicts with enchanced the interface info
+        """
+        for interface in interfaces:
+            interface.update(profiles[interface["profile"]])
+
+        return interfaces
